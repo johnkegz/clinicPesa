7ef6511478a540b1ed65b0a96b396082cb7eb909
johnkegz <johnkal24@gmail.com>
2025-11-19 14:14:18 +0300

    add ClinicPesaLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import './App.css'
 import ClinicPesaLayout from './pages/ClinicPesaLayout'
 
 function App() {
 
   return (
-    <div className="flex items-center justify-center text-green-500 p-4 rounded-lg">
+    <div className="App max-w-sm mx-auto shadow-xl min-h-screen">
       <ClinicPesaLayout />
     </div>
   )
--- src/pages/ClinicPesaLayout.tsx
+++ src/pages/ClinicPesaLayout.tsx
@@ -4,7 +4,7 @@ const ClinicPesaLayout = () => {
   
 
   return (
-    <div className="flex items-center justify-center text-green-500 p-4 rounded-lg">
+    <div>
       Hello World
     </div>
   );
